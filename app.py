7ae294708d36e020aa1a42c76284d508e06368ef
OKnows <114944966+OKnows@users.noreply.github.com>
2023-04-06 19:34:02 -0700

    final push

--- app.py
+++ app.py
@@ -7,19 +7,16 @@ app=Flask(__name__)
 #connect to database
 engine = create_engine('sqlite:///data/db.sqlite')
 
-@app.route('/data')
-def data():
-    return jsonify(['a','b','c'])
-
 #define routes
 @app.route('/')
 def index():
-    return render_template('index_pie.html')
+    return render_template("dashboard.html")
+
 
 @app.route('/heatmap')
 def heatmap():
     conn = engine.connect()
-    result = conn.execute("SELECT Country, `population of country`, City, AirQuality, WaterPollution, `population of city`, lat, lng, `proportion of population`, `Mortality rate of country (per 100,000)`, `Gross domestic product ($ billions)`, `Agriculture (% of GDP)`, `Industry (% of GDP)`, `Manufacturing (% of GDP)`, `Services (% of GDP)`, `Weighted mortality rate of city (per 100,000)` FROM data")
+    result = conn.execute("SELECT * FROM data")
     data_dict = {}
     for row in result:
         country = row[0]
@@ -55,12 +52,8 @@ def heatmap():
                               'services': services,
                               'weighted_mortality_rate': weighted_mortality_rate}
     conn.close()
-    data_dict = {country: {key: None if value == '..' else (float(value.replace(',', '')) if key in ['country_population', 'air_quality', 'water_pollution', 'city_population', 'lat', 'lng', 'percentage_population', 'country_mortality_rate', 'gdp', 'agriculture', 'industry', 'manufacturing', 'services', 'weighted_mortality_rate'] and isinstance(value, str) else float(value)) if key not in ['country', 'city'] else value
-                       for key, value in row.items() if value is not None}
-            for country, row in data_dict.items()}
-    
     return jsonify(data_dict)
-
+    
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
